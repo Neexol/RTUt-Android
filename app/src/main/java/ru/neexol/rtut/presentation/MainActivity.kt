86ec72ab15c74b369912ddb807a1b0c83d1901a2
Nikita <almostroll@yandex.ru>
2022-05-06 21:52:50 +0300

    Add simple design for lessons of group

--- app/src/main/java/ru/neexol/rtut/presentation/MainActivity.kt
+++ app/src/main/java/ru/neexol/rtut/presentation/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.neexol.rtut.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
 import ru.neexol.rtut.presentation.grouplessons.GroupLessonsList
@@ -14,7 +17,11 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			RTUtTheme {
-				GroupLessonsList(vm = viewModel())
+				Surface(
+					modifier = Modifier.fillMaxSize()
+				) {
+					GroupLessonsList(vm = viewModel())
+				}
 			}
 		}
 	}
